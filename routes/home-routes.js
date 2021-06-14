116af4be35cbb6b83876a7f09fe45ec4e130ac0b
Lara Coding <78417151+JoyofCodingLife@users.noreply.github.com>
2021-06-14 21:54:09 +1000

    working on handlebars

--- routes/home-routes.js
+++ routes/home-routes.js
@@ -1,5 +1,6 @@
 const router = require("express").Router();
 const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
@@ -16,7 +17,7 @@ router.get("/", async (req, res) => {
 
     res.render("homepage", {
       posts,
-      loggedIn: req.session.loggedIn,
+      logged_in: req.session.logged_in,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -38,7 +39,26 @@ router.get("/post/:id", async (req, res) => {
 
     res.render("post", {
       ...post,
-      loggedIn: req.session.loggedIn,
+      logged_in: req.session.logged_in,
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+router.get("/dashboard", withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ["password"] },
+      include: [{ model: Post }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render("dashboard", {
+      ...user,
+      logged_in: true,
     });
   } catch (err) {
     res.status(500).json(err);
@@ -46,7 +66,7 @@ router.get("/post/:id", async (req, res) => {
 });
 
 router.get("/login", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
   }
@@ -55,7 +75,7 @@ router.get("/login", (req, res) => {
 });
 
 router.get("/signup", (req, res) => {
-  if (req.session.loggedIn) {
+  if (req.session.logged_in) {
     res.redirect("/dashboard");
     return;
   }
